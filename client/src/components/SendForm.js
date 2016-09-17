8fcfa85bccc278afc316e745a023c9835f4f3d55
Arthelon <hsing.daniel@gmail.com>
2016-09-17 19:24:24 +0800

    Initialized SendForm component

--- client/src/components/SendForm.js
+++ client/src/components/SendForm.js
@@ -1,12 +1,51 @@
+/* eslint-disable */
 import React, { Component } from 'react'
 import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton'
 import reactCSS from 'reactcss'
+import {PAYPAL_TOKEN} from '../constants/config'
+import {createFetch, base, header, method, json} from 'http-client'
 
 export default class SendForm extends Component {
 
+
+
     state = {
         val: {}
+    }
+
+    makeTransaction = () => {
+        const fetch = createFetch(
+            base("https://api.sandbox.paypal.com"),
+            method("POST"),
+            header("Authorization", "Bearer " + PAYPAL_TOKEN),
+            json({
+                intent: "sale",
+                payer: {
+                    payment_method: "paypal"
+                },
+                redirect_urls: {
+                    return_url: 'http://return_URL_here',
+                    cancel_url: 'http://return_URL_here'
+                },
+                transactions: [
+                    {
+                        amount: {
+                            total: this.state.val.amount.toString(),
+                            currency: "HKD"
+                        }
+                    }
+                ]
+            })
+        )
+        fetch("/v1/payments/payment").then(resp => {
+            const data = resp.body
+            if (data.status == "created") {
+
+            }
+        }, err => {
+            console.log(err)
+        })
     }
 
     handleIdChange = (e) => {
@@ -84,7 +123,7 @@ export default class SendForm extends Component {
                     errorText={this.state.errorPassword}
                 /><br />
                 <TextField
-                    hintText="Amount"
+                    hintText="Amount (HKD)"
                     onChange={this.handleAmountChange}
                     errorText={this.state.errorAmount}
                 /><br/>
@@ -93,9 +132,7 @@ export default class SendForm extends Component {
                         !(this.state.errorPassword === "") && !(this.state.errorAmount === "") &&
                         !(this.state.errorId === "")
                     }
-                    onClick={() => {
-                        console.log(this.state.val)
-                }}/>
+                    onClick={this.makeTransaction}/>
             </div>
         )
     }
--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,2 +1,1 @@
-const PAYPAL_CLIENT_ID = ""
-const PAYPAL_CLIENT_SECRET = ""
+export const PAYPAL_TOKEN = "A101.RCyfgyR8RryeIlxFMuR-3Yk3Z0Jw3TYzvKGiOxI5Dh5otjqsHeYV4F1jWd_CzjRe.tC0AefXMXqlZ4UQb0LnHWAJMgjC"
